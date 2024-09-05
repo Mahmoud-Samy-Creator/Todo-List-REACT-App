edd45dde19fc0fa285c03deabcdb2d3327ee8e73
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-09-05 15:55:44 +0300

    Used static variables

---
+++ src/TodoList/staticVars/StaticVars.js
@@ -0,0 +1,76 @@
+// Control the application language
+// 1) Arabic language
+const arabicLang = {
+    direction: "rtl",
+    textAligning: "right",
+    fontFamilyType: "Beiruti",
+    header: 'مهامي',
+    toggleButtons: {
+        all: "الكل",
+        completed: "منجز",
+        notCompleted: "غير منجز"
+    },
+    todoAddForm: {
+        label: "عنوان المهمة",
+        button: "اضافة المهمة",
+        confirmMessage: "تم اضافة المهمة بنجاح"
+    },
+    todoPropEdit: {
+        header: "تعديل المهمة",
+        label: {
+            name: "اسم المهمة",
+            disc: "وصف المهمة"
+        },
+        buttons: {
+            close: "إغلاق",
+            edit: "تعديل"
+        }
+    },
+    todoDelete: {
+        header: "هل انت متأكد من رغبتك في حذف المهمة ؟",
+        message: "لا يمكنك التراجع عن الحذف بعد اتمامه",
+        buttons: {
+            close: "إغلاق",
+            delete: "تأكيد الحذف"
+        }
+    }
+}
+
+// 1) English language
+const englishLang = {
+    textAligning: "left",
+    direction: "ltr",
+    fontFamilyType: "SUSU",
+    header: "Todo's",
+    toggleButtons: {
+        all: "all",
+        completed: "done",
+        notCompleted: "not done"
+    },
+    todoAddForm: {
+        label: "to do name",
+        button: "Add",
+        confirmMessage: "To-do added succesfully"
+    },
+    todoPropEdit: {
+        header: "Edit todo",
+        label: {
+            name: "name",
+            disc: "details"
+        },
+        buttons: {
+            close: "Close",
+            edit: "Edit"
+        }
+    },
+    todoDelete: {
+        header: "?! Are you sure you want to delete this to do",
+        message: "You can't undo this action",
+        buttons: {
+            close: "Close",
+            delete: "Confirm delete"
+        }
+    }
+}
+
+export { arabicLang, englishLang };
---
+++ src/TodoList/staticVars/StylingVars.js
@@ -0,0 +1,23 @@
+// Card styling
+const cardStyling = { textAlign: "center", maxHeight: "80vh", overflowY: "scroll", position: "relative" }
+
+// Styles declarations
+const buttonStyle = {
+    color: "#f63a3a",
+    fontFamily: "Beiruti"
+}
+
+// Confirmation message styling
+const confirmationMessageStyle = {
+    position: 'fixed',
+    backgroundColor: "#65bfb4",
+    padding: "10px 40px",
+    bottom: "50px",
+    left: "50px",
+    borderRadius: "5px",
+    transition: "opacity 0.3s ease",
+    opacity: 0,
+    visibility: "hidden"
+};
+
+export { buttonStyle, confirmationMessageStyle, cardStyling };
